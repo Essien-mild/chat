a9137c5c007f550ee4f4be62deed73ce2539d102
Essien <essienetim4002@gmail.com>
2025-09-27 02:43:19 +0200

    2 commit

--- ChatPrj/ChatPrj/settings.py
+++ ChatPrj/ChatPrj/settings.py
@@ -23,9 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ye^-o#*fu5e-bn@fexkmr*%_8n_z&+lawno+gb*c*0)y^l@^zh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['yourusername.pythonanywhere.com']
+DEBUG = False
 
 
 # Application definition
